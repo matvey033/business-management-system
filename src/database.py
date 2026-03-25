ab77b2972ac7c6302e9c4f4c9b0e4a7116dfd1d9
matvey033 <mat.gol20003@gmail.com>
2026-03-25 10:06:13 +0300

    chore: setup database, sqlalchemy and async alembic

---
+++ src/database.py
@@ -0,0 +1,12 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+
+from src.core.config import settings
+
+engine = create_async_engine(settings.DATABASE_URL, echo=True)
+
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+
+class Base(DeclarativeBase):
+    pass
